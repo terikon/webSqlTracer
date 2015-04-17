3bd361580b3394f5e104caaa130b0784d2ff036c
viskin <r.viskin@terikon.com>
2015-04-17 13:35:56 +0300

    improved build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,25 @@
 module.exports = function(grunt) {
 	
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	
 	grunt.initConfig({
+		
+		pkg: grunt.file.readJSON('package.json'),
+		
 		uglify: {
-			target1: {
-				options: {
-					sourceMap: true
-				},
+			options: {
+				sourceMap: true,
+				banner: '/* <%= grunt.task.current.target %> v<%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> (C) 2015 Terikon Software */\n'
+			},
+			
+			webSqlTracer: {
 				src: 'webSqlTracer.js',
 				dest: 'dist/webSqlTracer.min.js'
 			}
+		},
+		jshint: {
+			all: ['Gruntfile.js', 'webSqlTracer.js']
 		}
 	});
 	
